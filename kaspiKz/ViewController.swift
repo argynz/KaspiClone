88cd88587785d1ab33787c5cb550dee935b686bf
shhk1per <argun.zhienbaev@gmail.com>
2023-11-15 16:47:19 +0600

    Photo Selection

--- kaspiKz/ViewController.swift
+++ kaspiKz/ViewController.swift
@@ -1,19 +1,44 @@
-//
-//  ViewController.swift
-//  kaspiKz
-//
-//  Created by Argyn on 02.11.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.image.layer.borderWidth = 1
+        self.image.layer.borderColor = UIColor.clear.cgColor
+        self.image.layer.masksToBounds = false
+        self.image.layer.cornerRadius = image.frame.size.height/2
+        self.image.clipsToBounds = true
+        
+        AddGusture()
     }
-
-
+    
+    func AddGusture(){
+        let tap = UITapGestureRecognizer(target: self, action: #selector (ImageTapped))
+        self.image.isUserInteractionEnabled = true
+        self.image.addGestureRecognizer(tap)
+    }
+    
+    @objc func ImageTapped(){
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let picking = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        self.image.image = picking
+        
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+    }
